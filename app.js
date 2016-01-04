4ee717de51c9d3cadae8a5a8ddef3b9089b9408b
Takeshi <a86487817@gmail.com>
2016-01-05 01:15:13 +0800

    Remove sample file and config

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var web_index = require('./routes/index');
+var upload_api = require('./routes/api');
 
 var app = express();
 
@@ -22,8 +22,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/', web_index);
+app.use('/api', upload_api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
