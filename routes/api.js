090370c754c760943f676a407b3298321c077f75
Takeshi <a86487817@gmail.com>
2016-01-06 02:44:56 +0800

    Implement /api/files API

--- routes/api.js
+++ routes/api.js
@@ -21,9 +21,21 @@ router.get('/', function(req, res, next) {
   res.render('api_doc', { title: 'Express' });
 });
 
-router.get('/files/:fid', function(req, res, next) {
+router.get('/files', function(req, res, next) {
   // list all files
-  // if fid is not undefined, download it.
+  let conn = req.app.get('db_conn');
+  let gfs = Grid(conn.db, mongoose.mongo);
+  gfs.files.find().toArray(function(err, files) {
+    if(err) {
+      res.status(500).json({msg: 'inter server error', err: true, result: []});
+      res.end();
+    } else {
+      res.status(200).json({msg: 'ok', err: false, result: files});
+      res.end();
+    }
+  });
+});
+
 });
 
 router.post('/upload', uploadMiddleware.single('file'), function(req, res, next) {
