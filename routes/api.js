c1ffe0ba457f0b0d146a55d16d3401b39d362875
Takeshi <a86487817@gmail.com>
2016-01-06 02:03:22 +0800

    Implement upload api

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,12 @@
+var fs = require('fs');
 var express = require('express');
 var router = express.Router();
+var multer  = require('multer')
+var uploadMiddleware = multer({ dest: 'tmp_files/' })
+var mongoose = require('mongoose');
+var Grid = require('gridfs-stream');
+var dbconfig = require(__dirname + '/../config.js');
+
 
 /*
  * api location start at /api
@@ -13,8 +20,22 @@ route.get('/files/:fid', function(req, res, next) {
     // if fid is not undefined, download it.
 });
 
-route.post('/upload', function(req, res, next) {
+router.post('/upload', uploadMiddleware.single('file'), function(req, res, next) {
     // upload a file
+    if(!req.file) {
+        res.status(400).json({msg: 'File is undefined.', err: true});
+        res.end();
+    }
+    var filePath = req.file.path;
+    var fileName = req.file.filename;
+    var conn = mongoose.createConnection(dbconfig.url);
+    conn.once('open', function () {
+        var gfs = Grid(conn.db, mongoose.mongo);
+        var writestream = gfs.createWriteStream({filename: fileName});
+        fs.createReadStream(filePath).pipe(writestream);
+        conn.close();
+        res.status(201).json({msg: 'done', err: false});
+    });
 });
 
 route.delete('files/:fid', function(req, res, next) {
