80ed142702a55f7dcbd3b0fb3521a1498dc930a1
Takeshi <a86487817@gmail.com>
2016-01-06 02:25:18 +0800

    Fix Error
    Original code might cause “Can't set headers after they are sent.”
    error.

--- routes/api.js
+++ routes/api.js
@@ -32,15 +32,15 @@ router.post('/upload', uploadMiddleware.single('file'), function(req, res, next)
         // file empty
         res.status(400).json({msg: 'File is undefined.', err: true});
         res.end();
+    } else {
+        let filePath = req.file.path;
+        let fileName = req.file.originalname;
+        let conn = req.app.get('db_conn');
+        let gfs = Grid(conn.db, mongoose.mongo);
+        let writestream = gfs.createWriteStream({filename: fileName});
+        fs.createReadStream(filePath).pipe(writestream);
+        res.status(201).json({msg: 'file created', err: false});
     }
-    let filePath = req.file.path;
-    let fileName = req.file.originalname;
-    let conn = req.app.get('db_conn');
-    let gfs = Grid(conn.db, mongoose.mongo);
-    let writestream = gfs.createWriteStream({filename: fileName});
-    fs.createReadStream(filePath).pipe(writestream);
-    res.status(201).json({msg: 'file created', err: false});
-
 });
 
 router.delete('files/:fid', function(req, res, next) {
