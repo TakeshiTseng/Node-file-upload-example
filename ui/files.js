df5fb31cabe0ce1b952f0d9b6e0c91f41cd4b89f
Takeshi <a86487817@gmail.com>
2016-01-06 12:55:57 +0800

    Finish file upload react ui

--- ui/files.js
+++ ui/files.js
@@ -16,14 +16,64 @@ var FileList = React.createClass({
 var FileApp = React.createClass({
   render: function() {
     return (
-      <FileList files={this.state.files} />
+      <div>
+        <form onSubmit={this.handleSubmit} encType="multipart/form-data" action="/api/upload" method="POST">
+          <input name="file" type="file" onChange={this.handleFileChange} />
+          <input name="submit" type="submit" />
+        </form>
+        <FileList files={this.state.files} />
+      </div>
     );
   },
   getInitialState: function() {
-    return {files: []}
+    return {
+      files: [],
+      fileToUpload: null
+    }
+  },
+  updateFileList: function() {
+    $.get('/api/files', function(res) {
+      if(res.err) {
+        this.setState({
+          files: []
+        });
+      } else {
+        this.setState({
+          files: res.result
+        });
+      }
+    }.bind(this));
   },
   handleSubmit: function(e) {
     e.preventDefault();
+    var data = new FormData();
+    data.append('file', this.state.fileToUpload);
+
+    $.ajax({
+        url: '/api/upload',
+        type: 'POST',
+        data: data,
+        cache: false,
+        dataType: 'json',
+        processData: false, // Don't process the files
+        contentType: false, // Set content type to false as jQuery will tell the server its a query string request
+        success: function(data, msg, jqXHR) {
+            if(typeof data.error === 'undefined')
+            {
+              this.updateFileList();
+            } else {
+                console.log('Error: ' + data.error);
+            }
+        }.bind(this),
+        error: function(jqXHR, msg, err) {
+          console.log("Error: " + msg);
+        }.bind(this)
+    });
+  },
+  handleFileChange: function(e) {
+    var reader = new FileReader();
+    var file = e.target.files[0];
+    this.setState({fileToUpload: e.target.files[0]});
   },
   componentDidMount: function() {
     $.get('/api/files', function(res) {
